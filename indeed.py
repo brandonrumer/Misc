3fa4ed22e4297a5bfc1b0336045389be0c4ee2dc
brandonrumer <44004200+brandonrumer@users.noreply.github.com>
2020-11-18 09:34:37 -0500

    x

--- indeed.py
+++ indeed.py
@@ -68,8 +68,8 @@ def main():
     totaljobs = totaljobs.replace(',','')
 
     # We have the total jobs, get the total pages to search through, in sections of 10
-    pages = int(math.ceil(int(totaljobs) / 10.0))
-
+    pages = round(int(totaljobs) / int(10))
+  
 
     jobs = []
     # Ugly way to do this but its quick
