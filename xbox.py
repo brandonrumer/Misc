7f60e3301b2df71a64767b9614eb7dfecaaf230a
brandonrumer <44004200+brandonrumer@users.noreply.github.com>
2020-11-28 21:55:03 -0500

    fixed beep function usage

--- xbox.py
+++ xbox.py
@@ -9,9 +9,10 @@ Best Buy, Target, and Walmart actually have APIs but you need to be registered
  UPDATE: I found that popfindr.com can pull inventory, and then I can statically
  scrape inventory from the results. This is for the Target and Best Buy inventory
  in Fayetteville NC. Use Firefox Developer to pull the right page from PopFindr.
-
+ 
  Added Microsoft.com inventory scrape. 
  
+
  For the sound to work in Linux under Python3: You need: 'sudo apt install sox' on a 
  Debian-based system so that the system can 'beep' at you when the item is found.
 '''
@@ -22,6 +23,7 @@ from datetime import datetime
 import pandas as pd
 # due to a bug in Windowz, you'll need 'pip install numpy==1.19' for pandas
 import platform 
+import winsound
 
 
 def main():
@@ -33,11 +35,11 @@ def main():
             current_time = now.strftime("%H:%M:%S")
             print("Current Time: ", current_time)
 
-            PopFindrBestBuy()
-            PopFindrTarget()
-            microsoft()
-            bhphoto()
-            newegg()
+            PopFindrBestBuy(operatingsystem)
+            PopFindrTarget(operatingsystem)
+            microsoft(operatingsystem)
+            bhphoto(operatingsystem)
+            newegg(operatingsystem)
             print('\n')
             print('Sleeping for 60 seconds')
             time.sleep(60)
@@ -74,17 +76,18 @@ def getplatform():
 
 
 def beep(operatingsystem):
-    if operatingsystem == 'linux':
+    if operatingsystem == 'Linux':
         duration = 20  # seconds
         freq = 440  # Hz
         os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
-    if operatingsystem =='windows':
+    if operatingsystem =='Windows':
+        print('beeping windows!')
         for i in range(1,10):
             winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
 
 
 
-def newegg():
+def newegg(operatingsystem):
     #print('Checking NewEgg')
     try:
         producturl = 'https://www.newegg.com/p/N82E16868105273'
@@ -103,7 +106,7 @@ def newegg():
         print('Ran into an exception in NewEgg. Likely a request error.')
 
 
-def bhphoto():
+def bhphoto(operatingsystem):
     #print('Checking BHPhotoVideo')
     try:
         #producturl = 'https://www.bhphotovideo.com/c/product/1573218-REG/microsoft_234_00001_xbox_one_s_1tb.html'
@@ -126,7 +129,7 @@ def bhphoto():
         print('Ran into an exception in bhphoto. Likely a request error.')        
 
 
-def PopFindrTarget():
+def PopFindrTarget(operatingsystem):
     producturl = 'https://popfindr.com/results?pid=207-41-0001&zip=28348&range=25&webpage=target&token=03AGdBq25xvIZKDLjisiS4_S--HVo_I0H9oBXwwy_RPqFAu4dosj6ThW_rh1aGOZIjWZKY2hM0aM-MzL7Z40PrumdmaPUnkSBTG5i5tNezWCsjxQ4BX4VghDWdB1OFJSJS2WlmH0S-HTIfNnZWwbJ2rLG1sWsSf6oAtBr9oM73i_oWI3SVCQnRCXjA5bK3nMppvtWmAp6UXA40QslzFr-dqc6oadbohkkHjezScChv1cjDP70RzWOYDsr4Qi5SVKezFc0ZnkayUOd1cGW65_OeVT19DZd7S1ShubhVIsLRry0ZYQ5xdbqIPkHas3ren5O34UIWFDMMCR15m_YtcufuXbaL8JxcdBPHUWaFgtl7K7UDqyuVGBw-QRt14lJXAeBudgw0-yVJTGmpkYPcCe_493ePWBN0TbsC3sNua6dGlVNUymESm7BteffBV3ztTyX8ihjhCwwO6F3oBCbu05pWFQie513TA6o-qlI5hwydRQHkWgJ-WkeV6kA'
     try:
         page = requests.get(producturl)
@@ -149,7 +152,7 @@ def PopFindrTarget():
         print('Quitting...')
         quit()
 
-def microsoft():
+def microsoft(operatingsystem):
     producturl ='https://www.xbox.com/en-us/configure/8WJ714N3RBTL?ranMID=24542&ranEAID=kXQk6*ivFEQ&ranSiteID=kXQk6.ivFEQ-WT4rQZgrrl6u7WL9k7aH9A&epi=kXQk6.ivFEQ-WT4rQZgrrl6u7WL9k7aH9A&irgwc=1&OCID=AID2000142_aff_7593_1243925&tduid=%28ir__gce1vdpse0kfqibbkk0sohzn3m2xsu1wbx0bflsr00%29%287593%29%281243925%29%28kXQk6.ivFEQ-WT4rQZgrrl6u7WL9k7aH9A%29%28%29&irclickid=_gce1vdpse0kfqibbkk0sohzn3m2xsu1wbx0bflsr00'
     try:
         page = requests.get(producturl)
@@ -170,7 +173,7 @@ def microsoft():
         print('Quitting...')
         quit()
 
-def PopFindrBestBuy():
+def PopFindrBestBuy(operatingsystem):
     producturl = 'https://popfindr.com/results?pid=6428324&zip=28348&range=25&webpage=bestbuy&token=03AGdBq27W1_OQsSNJW8bf-JBDrP8rU5xOwMy_Mi65vTZvU0RrYe-FBmslESjnNN2fh6ODHOVcxkl3jFMTdP8BV9-z0n2rp_5gxWPt32-IpmEhBK73oixHOvvsUKZM08S40xCLuPMt6oRYN8-X3zkOMBi1muqAs7Oy1IgDkYzX53zl12uXbEF712T-qMy0yCDJDKpOALYCHDWmXrrrkY8sQGUvZbsg0JfPHBo4bno3j9D0hx0iJ3z-BAJKkbvmmOve0yKuk-H91xP4G2ESTANHHVQKcrijxciAw3QNn8Px-yfBY6t232-FcpEsVv_5-KC9d96TE52o652tldztV-YQlRbVFTChJHkPH-ErF2x1ehKNis6PAcbhG4ywhcX9LgEp6eyqfNyh0oYwurPAdtojd5XFo_yoRvCSx7iun1AALIkrDSC4rLZMfiXU5OMuMYHwSHD_3BSsYQ4M7xBMy1AlwaqtbQ2Czvf_tQ'
     try:
         page = requests.get(producturl)
