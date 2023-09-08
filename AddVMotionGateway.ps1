739da7ee8d98347f36ba08e77f919593b4234908
brandonrumer <44004200+brandonrumer@users.noreply.github.com>
2023-09-08 13:42:08 -0400

    Add files via upload

---
+++ AddVMotionGateway.ps1
@@ -0,0 +1,27 @@
+﻿$esxName = 'MyEsx'
+
+$vmkName = 'vmk1'
+
+$esx = Get-VMHost -Name $esxName
+
+$esxcli = Get-EsxCli -VMHost $esx -V2
+
+$if = $esxcli.network.ip.interface.ipv4.get.Invoke(@{interfacename=$vmkName})
+
+$iArg = @{
+
+    netmask = $if[0].IPv4Netmask
+
+    type    = $if[0].AddressType.ToLower()
+
+    ipv4    = $if[0].IPv4Address
+
+    interfacename = $if[0].Name
+
+    gateway = '192.168.10.222'
+
+}
+
+$esxcli.network.ip.interface.ipv4.set.Invoke($iArg)
+
+$esxcli.network.ip.interface.ipv4.get.Invoke(@{interfacename=$vmkName})
